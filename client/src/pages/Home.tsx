c78b0ab5dd5590f499cd680d271dce8a32f573f9
mllabresf-dotcom <mllabresf@esliceu.net>
2025-10-29 11:34:52 +0100

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,1 +1,19 @@
+import Hero from "@/components/Hero";
+import ServicesGrid from "@/components/ServicesGrid";
+import ProcessSteps from "@/components/ProcessSteps";
+import ProductsPreview from "@/components/ProductsPreview";
+import QuoteCalculator from "@/components/QuoteCalculator";
+import Testimonials from "@/components/Testimonials";
 
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <ServicesGrid />
+      <ProcessSteps />
+      <ProductsPreview />
+      <QuoteCalculator />
+      <Testimonials />
+    </div>
+  );
+}
